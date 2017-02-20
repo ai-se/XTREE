fac8705d8bc50473c7329b6e1fcc590e4a257e31
rahlk <i.m.ralk@gmail.com>
2017-02-19 23:49:59 -0500

    Autocommit

--- src/oracle/model.py
+++ src/oracle/model.py
@@ -5,7 +5,8 @@ import sys
 from pdb import set_trace
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-
+from Utils.FileUtil import list2dataframe
+from sklearn.metrics import roc_curve, roc_auc_score
 from smote import SMOTE
 
 root = os.path.join(os.getcwd().split('src')[0], 'src')
@@ -22,19 +23,32 @@ def getTunings(fname):
         return None
 
 
-def rforest(source, target):
+def rforest(train, target):
     clf = RandomForestClassifier(n_estimators=100, random_state=1)
-    try:
-        source = SMOTE(source)
-    except ValueError:
-        pass
+    source = list2dataframe(train)
+    pseudo_train = list2dataframe(train[:-1])
+    pseudo_test = list2dataframe(train[-1])
+
     features = source.columns[:-1]
     klass = source[source.columns[-1]]
-    set_trace()
     clf.fit(source[features], klass)
-    preds = clf.predict(target[target.columns[:-1]])
-    distr = clf.predict_proba(target[target.columns[:-1]])
-    return preds, distr[:, 1]
+    pred0 = clf.predict(target[target.columns[:-1]])
+    distr = clf.predict_proba(target[target.columns[:-1]])[:, 1]
+
+    # Find a threshold for cutoff
+    thresh = 0.5
+    klass0 = pseudo_train[pseudo_train.columns[-1]]
+    clf.fit(pseudo_train[features], klass0)
+    distribution = clf.predict_proba(pseudo_test[pseudo_test.columns[:-1]])
+    fpr, tpr, thresholds = roc_curve(pseudo_test[pseudo_test.columns[-1]], distribution[:, 1])
+    cutoff = 0.66
+    for a, b, c in zip(fpr, tpr, thresholds):
+        if b > cutoff and a < 1-cutoff:
+            thresh = c
+
+    # Apply cutoff
+    preds = [1 if val < thresh else 0 for val in distr]
+    return preds, distr
 
 
 def _test_model():
--- src/run.py
+++ src/run.py
@@ -17,23 +17,28 @@ from Utils.FileUtil import list2dataframe
 
 
 def secondary_verification(train, test, patched):
-    train = list2dataframe(train)
     test = list2dataframe(test)
     before = test[test.columns[-1]].values.tolist()
     after, distr = rforest(train, patched)
     return before, after, distr
 
 
+def impact(before, after):
+    return (1 - sum(before) / sum(after)) * 100
+
+
 def run_planner(n_reps=1):
     source, target = get_train_test()
     out = ["XTREE"]
     for train, test in zip(source, target):
+        res = [test.split('/')[-2].title()]
         for _ in xrange(n_reps):
             patched = execute(train, test)
-            before, after, distr = secondary_verification(train, test, patched)
-            set_trace()
-    return out
+            before, after, __ = secondary_verification(train, test, patched)
+            res.append(impact(before, after))
+        out.append(res)
 
+    set_trace()
 
 if __name__ == "__main__":
     seed(1)
