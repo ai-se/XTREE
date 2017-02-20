7b30f73f2c445d6c3f062f39d2aca50ef3216c5e
rahlk <i.m.ralk@gmail.com>
2017-02-19 19:52:38 -0500

    Autocommit

---
+++ src/XTREE/XTREE.py
@@ -0,0 +1,160 @@
+"""
+XTREE
+"""
+from __future__ import print_function, division
+
+import os
+import sys
+
+# Update path
+root = os.path.join(os.getcwd().split('src')[0], 'src')
+if root not in sys.path:
+    sys.path.append(root)
+
+from tools.sk import *
+from tools.oracle import *
+import tools.pyC45 as pyC45
+from random import uniform
+
+
+def trueValue(all, test):
+    set_trace()
+
+
+def flatten(x):
+    """
+    Takes an N times nested list of list like [[a,b],[c, [d, e]],[f]]
+    and returns a single list [a,b,c,d,e,f]
+    """
+    result = []
+    for el in x:
+        if hasattr(el, "__iter__") and not isinstance(el, basestring):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
+
+
+class changes():
+    """
+    Record changes.
+    """
+
+    def __init__(self):
+        self.log = {}
+
+    def save(self, name=None, old=None, new=None):
+        if not old == new:
+            delt = new / old  # if old>0 else 0
+            self.log.update({name: delt})
+
+
+class patches:
+    def __init__(i, train, test, trainDF, testDF, tree=None, config=False):
+        i.train = train
+        i.trainDF = trainDF
+        i.test = test
+        i.testDF = testDF
+        i.config = config
+        i.tree = tree
+        i.change = []
+
+    def leaves(i, node):
+        """
+        Returns all terminal nodes.
+        """
+        L = []
+        if len(node.kids) > 1:
+            for l in node.kids:
+                L.extend(i.leaves(l))
+            return L
+        elif len(node.kids) == 1:
+            return [node.kids]
+        else:
+            return [node]
+
+    def find(i, testInst, t):
+        if len(t.kids) == 0:
+            return t
+        for kid in t.kids:
+            if i.config:
+                if kid.val[0] == testInst[kid.f].values[0]:
+                    return i.find(testInst, kid)
+            else:
+                try:
+                    if kid.val[0] <= testInst[kid.f].values[0] < kid.val[1]:
+                        return i.find(testInst, kid)
+                    elif kid.val[1] == testInst[kid.f].values[0] == i.trainDF.describe()[kid.f]['max']:
+                        return i.find(testInst, kid)
+                except:
+                    set_trace()
+        return t
+
+    @staticmethod
+    def howfar(me, other):
+        common = [a for a in me.branch if a not in other.branch]
+        return len(me.branch) - len(common)
+
+    def patchIt(i, testInst, config=False):
+        # 1. Find where t falls
+        C = changes()  # Record changes
+        testInst = pd.DataFrame(testInst).transpose()
+        current = i.find(testInst, i.tree)
+        node = current
+        while node.lvl > -1:
+            node = node.up  # Move to tree root
+
+        leaves = flatten([i.leaves(_k) for _k in node.kids])
+        try:
+            if i.config:
+                best = sorted([l for l in leaves if l.score < current.score], key=lambda F: i.howfar(current, F))[0]
+            else:
+                best = \
+                sorted([l for l in leaves if l.score <= 0.01 * current.score], key=lambda F: i.howfar(current, F))[0]
+        except:
+            return testInst.values.tolist()[0]
+
+        def new(old, range):
+            rad = abs(min(range[1] - old, old - range[1]))
+            return uniform(range[0], range[1])
+
+        for ii in best.branch:
+            before = testInst[ii[0]]
+            if not ii in current.branch:
+                then = testInst[ii[0]].values[0]
+                now = ii[1] if i.config else new(testInst[ii[0]].values[0], ii[1])
+                # print(current.branch,best.branch)
+                testInst[ii[0]] = now
+                C.save(name=ii[0], old=then, new=now)
+
+        testInst[testInst.columns[-1]] = 1
+        i.change.append(C.log)
+        return testInst.values.tolist()[0]
+
+    def main(i):
+        newRows = [i.patchIt(i.testDF.iloc[n]) for n in xrange(i.testDF.shape[0]) if
+                   i.testDF.iloc[n][-1] > 0 or i.testDF.iloc[n][-1] == True]
+        return pd.DataFrame(newRows, columns=i.testDF.columns)
+
+
+def execute(train, test):
+    """XTREE"""
+
+    train_DF = csv2DF(train, toBin=True)
+    test_DF = csv2DF(test)
+    tree = pyC45.dtree(train_DF)
+    set_trace()
+    patch = patches(train=train, test=test, trainDF=train_DF, testDF=test_DF, tree=tree)
+    return patch.main()
+
+
+if __name__ == '__main__':
+    E = []
+    for name in ['ant']:  # , 'ivy', 'jedit', 'lucene', 'poi']:
+        print("##", name)
+        train, test = explore(dir='../Data/Jureczko/', name=name)
+        aft = [name]
+        for _ in xrange(10):
+            aft.append(xtree(train, test))
+        E.append(aft)
+    rdivDemo(E)
--- src/run.py
+++ src/run.py
@@ -8,76 +8,38 @@ root = os.path.join(os.getcwd().split('src')[0], 'src')
 if root not in sys.path:
     sys.path.append(root)
 
-import numpy as np
+from pdb import set_trace
 from Utils.FileUtil import create_tbl
 from oracle.model import rforest
+from Data.handler import get_train_test
+
 
 class main():
-    def __init__(self, _n=-1, smote=True, _tuneit=False, dataName=None, reps=12):
-
+    def __init__(self, _n=-1, smote=False, _tuneit=False, dataName=None, reps=12):
         self.dataName = dataName
         self.out = [self.dataName]
         self.out_pred = []
         self.rebalance = smote
-        self.train, self.test = self.categorize()
+        self.train, self.test = get_train_test
         self.reps = reps
         self._n = _n
 
     def secondary_verification(self, train, test):
         actual = test[test.columns[-1]].values.tolist()
-        predicted, distr = rforest(train, test, tunings=self.tunedParams, smoteit=True)
+        predicted, distr = rforest(train, test, tunings=True, smote=self.rebalance)
         return actual, predicted, distr
 
     def go(self):
-
         def newRows(newTab):
             return map(lambda Rows: Rows.cells[:-1], newTab._rows)
 
-        frac = lambda aft: 1 - (sum([0 if a < 1 else 1 for a in aft]) \
-                                / sum([0 if b < 1 else 1 for b in actual]))
-
-        for planner in ['xtrees']:
-            out = [planner]
-            for _ in xrange(self.reps):
-                predRows = []
-                train_DF = create_tbl(self.train[self._n], isBin=True)
-                test_df = create_tbl(self.test[self._n], isBin=True)
-                actual = np.array(Bugs(test_df))
-                before = self.pred(train_DF, test_df,
-                                   tunings=self.tunedParams,
-                                   smoteit=True)
-
-                predRows = [row.cells for row in create_tbl(
-                    self.test[self._n], isBin=True)._rows if row.cells[-2] > 0]
-
-                predTest = genTable(test_df, rows=predRows, name='Before_temp')
-
-                "Apply Different Planners"
-                if planner == 'xtrees':
-                    newTab = xtrees(train=self.train[-1],
-                                    test_DF=predTest,
-                                    bin=False,
-                                    majority=True).main()
-                    genTable(test_df, rows=newRows(newTab), name='After_xtrees')
-                # set_trace()
-                elif planner == 'XTREE':
-                    newTab = xtrees(train=self.train[-1],
-                                    test_DF=predTest,
-                                    bin=False,
-                                    majority=False).main()
-
-                elif planner == 'BIC':
-                    newTab = HOW(train=self.train[-1],
-                                 test=self.test[-1],
-                                 test_df=predTest).main()
-
-                elif planner == 'CD':
-                    newTab = strawman(train=self.train[-1], test=self.test[-1]).main()
-
-                elif planner == 'CD+FS':
-                    newTab = strawman(train=self.train[-1], test=self.test[-1]
-                                      , prune=True).main()
-
-                out.append(frac(after(newTab)))
-            # self.logResults(out)
-            yield out
+        out = ["XTREE"]
+        for _ in xrange(self.reps):
+            train_df = create_tbl(self.train, isBin=True)
+            test_df = create_tbl(self.test, isBin=True)
+            newTab = xtree(train=train_df,
+                           test_DF=test_df,
+                           bin=False,
+                           majority=True).main()
+        set_trace()
+        yield out
